9af636f69066ff3cfcf12bbc84481fbb70ac5ecd
ARCHER <gavigan.ryan@gmail.com>
2019-07-25 12:34:23 -0400

    fix header attempt

--- src/components/layout/Header.js
+++ src/components/layout/Header.js
@@ -5,7 +5,7 @@ function Header() {
   return(
     <header style={headerStyle}>
       <h1>TodoList</h1>
-      <Link style={linkStyle} to='/'>Home</Link> | <Link
+      <Link style={linkStyle} to='/react_crash_todo'>Home</Link> | <Link
       style={linkStyle} to='about'>About</Link>
     </header>
   )
